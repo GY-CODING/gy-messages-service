b4ed13e9411360d7cb2058083136a321cf8bebce
toxyc <ivi1000vm@gmail.com>
2024-12-27 19:45:45 +0100

    feat: limit access for origins based on the env

---
+++ src/main/java/org/gycoding/messages/infrastructure/config/CustomCORSFilter.java
@@ -0,0 +1,37 @@
+package org.gycoding.messages.infrastructure.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.gycoding.exceptions.model.APIException;
+import org.gycoding.messages.domain.exceptions.ChatAPIError;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class CustomCORSFilter extends OncePerRequestFilter {
+    @Value("${allowed.origin}")
+    private String allowedOrigin;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String origin = request.getHeader("Host");
+
+        if (origin != null && origin.startsWith(allowedOrigin)) {
+            filterChain.doFilter(request, response);
+        } else {
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            response.getWriter().write(
+                    new APIException(
+                        ChatAPIError.FORBIDDEN.getCode(),
+                        ChatAPIError.FORBIDDEN.getMessage(),
+                        ChatAPIError.FORBIDDEN.getStatus().value()
+                    ).toString()
+            );
+        }
+    }
+}
