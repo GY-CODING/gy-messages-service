b0c31b07811398ac9dc55c2e70e0f5f2a58cdb75
ToxYc <ivi1000vm@gmail.com>
2025-01-03 00:40:09 +0100

    fix: fix approach on cors filter

--- src/main/java/org/gycoding/messages/infrastructure/config/CustomCORSFilter.java
+++ src/main/java/org/gycoding/messages/infrastructure/config/CustomCORSFilter.java
@@ -14,28 +14,25 @@ import java.io.IOException;
 
 @Component
 public class CustomCORSFilter extends OncePerRequestFilter {
-    @Value("${allowed.origin}")
-    private String allowedOrigin;
-
     @Value("${allowed.apiKey}")
     private String allowedApiKey;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        String origin = request.getHeader("Host");
         String apiKey = request.getHeader("x-api-key");
 
-        if ((origin != null && origin.startsWith(allowedOrigin)) || (apiKey != null && apiKey.equals(allowedApiKey))) {
+        if (apiKey != null && apiKey.equals(allowedApiKey)) {
             filterChain.doFilter(request, response);
         } else {
             response.setStatus(HttpServletResponse.SC_FORBIDDEN);
             response.getWriter().write(
                     new APIException(
-                        ChatAPIError.FORBIDDEN.getCode(),
-                        ChatAPIError.FORBIDDEN.getMessage(),
-                        ChatAPIError.FORBIDDEN.getStatus().value()
+                            ChatAPIError.FORBIDDEN.getCode(),
+                            ChatAPIError.FORBIDDEN.getMessage(),
+                            ChatAPIError.FORBIDDEN.getStatus().value()
                     ).toString()
             );
         }
     }
 }
+
