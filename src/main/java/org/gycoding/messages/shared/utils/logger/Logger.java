582f4f4dbbe747284015221f1eb963fffd6d6961
ToxYc <ivi1000vm@gmail.com>
2025-03-29 02:29:30 +0100

    feat: add lombok logs to logger

--- src/main/java/org/gycoding/messages/shared/utils/logger/Logger.java
+++ src/main/java/org/gycoding/messages/shared/utils/logger/Logger.java
@@ -1,6 +1,7 @@
 package org.gycoding.messages.shared.utils.logger;
 
 import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
 import org.gycoding.messages.infrastructure.external.unirest.UnirestFacade;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Component;
 import java.util.Map;
 
 @Component
+@Slf4j
 public class Logger {
     @Value("${gy.logs.url}")
     private String initialUrl;
@@ -28,31 +30,39 @@ public class Logger {
         final var headers = Map.of(
                 "Authorization", token
         );
+        final var content = String.format("{\"level\": \"%s\", \"message\": \"%s\", \"data\": %s}", LogLevel.INFO, message, data);
 
-        UnirestFacade.post(url, headers, String.format("{\"level\": \"%s\", \"message\": \"%s\", \"data\": %s}", LogLevel.INFO, message, data));
+        log.info(content);
+        UnirestFacade.post(url, headers, content);
     }
 
     public static void debug(String message, Object data) {
         final var headers = Map.of(
                 "Authorization", token
         );
+        final var content = String.format("{\"level\": \"%s\", \"message\": \"%s\", \"data\": %s}", LogLevel.DEBUG, message, data);
 
-        UnirestFacade.post(url, headers, String.format("{\"level\": \"%s\", \"message\": \"%s\", \"data\": %s}", LogLevel.DEBUG, message, data));
+        log.debug(content);
+        UnirestFacade.post(url, headers, content);
     }
 
     public static void warn(String message, Object data) {
         final var headers = Map.of(
                 "Authorization", token
         );
+        final var content = String.format("{\"level\": \"%s\", \"message\": \"%s\", \"data\": %s}", LogLevel.WARN, message, data);
 
-        UnirestFacade.post(url, headers, String.format("{\"level\": \"%s\", \"message\": \"%s\", \"data\": %s}", LogLevel.WARN, message, data));
+        log.warn(content);
+        UnirestFacade.post(url, headers, content);
     }
 
     public static void error(String message, Object data) {
         final var headers = Map.of(
                 "Authorization", token
         );
+        final var content = String.format("{\"level\": \"%s\", \"message\": \"%s\", \"data\": %s}", LogLevel.ERROR, message, data);
 
-        UnirestFacade.post(url, headers, String.format("{\"level\": \"%s\", \"message\": \"%s\", \"data\": %s}", LogLevel.ERROR, message, data));
+        log.error(content);
+        UnirestFacade.post(url, headers, content);
     }
 }
